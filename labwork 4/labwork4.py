a77af9ab22bdf870890580dc2bc74f8a31a25778
NhatAnhNg <anhnn2440057@usth.edu.vn>
2025-05-10 13:33:31 +0700

    Update class for Neural

--- labwork 4/labwork4.py
+++ labwork 4/labwork4.py
@@ -2,37 +2,32 @@ import random
 import math
 
 def load_data(filename):
-    with open (filename, 'r') as file:
+    with open(filename, 'r') as file:
         lines = file.readlines()
-    file.close()
     return [int(line.strip()) for line in lines]
 
-class Layer:
-    def __init__(self, num_neurons, num_inputs):
-        self.num_neurons = num_neurons
-        self.num_inputs = num_inputs
-        self.weights = [[random.random() for _ in range(num_inputs)] for _ in range(num_neurons)]
-        self.bias = [random.random() for _ in range(num_neurons)]
-        
+class Neural:
+    def __init__(self, num_inputs):
+        self.weights = [random.random() for _ in range(num_inputs)]
+        self.bias = random.random()
+    
     def sigmoid(self, x):
         return 1 / (1 + math.exp(-x))
     
     def feedforward(self, inputs):
-        outputs = []
-        for i in range(self.num_neurons):
-            z = 0
-            for j in range(self.num_inputs):
-                z += (self.weights[i][j] * inputs[j])
-            z += self.bias[i]
-            outputs.append(self.sigmoid(z))
-        return outputs
+        z = sum(w * i for w, i in zip(self.weights, inputs)) + self.bias
+        return self.sigmoid(z)
+
+class Layer:
+    def __init__(self, num_neurons, num_inputs):
+        self.neurons = [Neural(num_inputs) for _ in range(num_neurons)]
+
+    def feedforward(self, inputs):
+        return [neuron.feedforward(inputs) for neuron in self.neurons]
 
 class NeuralNetwork:
     def __init__(self, layer_size):
-        self.layers = []
-        for i in range(1, len(layer_size)):
-            layer = Layer(layer_size[i], layer_size[i - 1])
-            self.layers.append(layer)
+        self.layers = [Layer(layer_size[i], layer_size[i - 1]) for i in range(1, len(layer_size))]
 
     def feedforward(self, data):
         for layer in self.layers:
@@ -44,7 +39,9 @@ num_layers = lines[0]
 neurons_layers = lines[1:]
 
 nn = NeuralNetwork(neurons_layers)
+
 input_data = [random.random() for _ in range(neurons_layers[0])]
 output = nn.feedforward(input_data)
+
 print("Input:", input_data)
 print("Output:", output)
